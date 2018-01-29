732b700b12acdb84a207e178069675ee74b00614
unknown <mikexross@gmail.com>
2018-01-28 21:26:30 -0700

    functional

---
+++ keys.js
@@ -0,0 +1,17 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'XPqE2N3BMBWjjLDUqDy3HUxIq',
+  consumer_secret: 'MN9JyKErs1Z5Uel3Yqrl0Nf4MJgUhsmxqjHpsSTSEYlDTPhy4P',
+  access_token_key: '956667003342749697-Kk8yG4STtYZpXc9IPmWx1jMv96IY55I',
+  access_token_secret: '8ZKOpbEImv9XPKVSWbYUiRrDQH3gTNbNWxEKRKteIrrjF',
+}
+// console.log(twitterKeys);
+var spotifyKeys = {
+  id: 'c2d1743637904841b0234fca6aad4110',
+  secret: '4fcf093be53745c099dea0db8da70656',
+}
+// console.log(spotifyKeys)
+
+module.exports.twitterKeys = twitterKeys;
+module.exports.spotifyKeys = spotifyKeys;
